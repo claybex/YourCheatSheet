a3d6615192d4403413b8d296031ace93ec23fc4a
Max <profex55@gmail.com>
2023-10-12 18:15:47 -0500

    global update

--- src/YourCheatSheet/cli.py
+++ src/YourCheatSheet/cli.py
@@ -1,56 +1,16 @@
-import json
-from markdown_it import MarkdownIt
-import markdown_to_json
+#!/usr/bin/env python3
+import markdown_to_json, pathlib, json 
+from sys import argv
+from main import Parser
 
-# Read the content of the markdown file
-with open('docs.md', 'r') as file:
-    markdown_text = file.read()
-    dictified = markdown_to_json(markdown_text)
-    print(dictified)
+path_str = argv[1]
+path = pathlib.Path(path_str)
+content = path.read_text()
+json_string = markdown_to_json.jsonify(content)
+json_object = json.loads(json_string)
+# print(json_object)
 
-
-# Initialize the Markdown parser
-md = MarkdownIt()
-
-# Parse the markdown text
-parsed = md.parse(markdown_text)
-
-# Initialize the JSON structure
-result = {}
-
-# Initialize a stack to keep track of headings and their levels
-stack = []
-
-# Iterate through the parsed Markdown tokens
-for token in parsed:
-    if token.type == 'heading_open':
-        # Get the heading level
-        heading_level = int(token.tag[1])
-        
-        # Get the heading text from the next token (heading_close)
-        heading_text = parsed[parsed.index(token) + 1].content
-        
-        # Pop stack until the appropriate level is reached
-        while len(stack) >= heading_level:
-            stack.pop()
-        
-        # Create a nested structure for the heading
-        nested_structure = result
-        for i in stack:
-            nested_structure = nested_structure[i]
-        
-        # Add the heading to the nested structure
-        if heading_text not in nested_structure:
-            nested_structure[heading_text] = {}
-        
-        # Append the heading to the stack
-        stack.append(heading_text)
-
-# Convert the result to a JSON string
-json_data = json.dumps(result, indent=2)
-
-# Print the JSON data
-print(json_data)
-
+docs = Parser(json_object)
+docs.main()
 
 
---
+++ src/YourCheatSheet/main.py
@@ -0,0 +1,49 @@
+from sys import argv
+
+class Parser:
+    def __init__(self,json):
+         self.json_object = json
+    def main(self):     
+        try:
+            if len(argv) == 3:
+                try:
+                    for key in self.json_object[argv[2]]:
+                        print(key)
+                except(KeyError, TypeError):
+                        print('Wrong topic was specified, try one of available bellow:'
+                            .format())
+                        for key in self.json_object:
+                            print(key)
+                
+            elif len(argv) == 4:
+                    try:    
+                        if any(key <= str(1) for key in self.json_object[argv[2]][argv[3]]):
+                            print(self.json_object[argv[2]][argv[3]])
+                        else:
+                            for key in self.json_object[argv[2]][argv[3]]:
+                                print(key)
+                    except(KeyError, TypeError):
+                        if argv[2] in self.json_object:
+                            print('Wrong topic was specified, try one of available bellow:'
+                                .format())
+                            for key in self.json_object[argv[2]]:
+                                print(key)
+                        else:
+                             print("Multiple keys error!")
+            elif len(argv) == 5:
+                try:
+                    print(self.json_object[argv[2]][argv[3]][argv[4]])
+                except(KeyError, TypeError):
+                        if argv[2] in self.json_object and argv[3] in self.json_object[argv[2]]:
+                            print('Wrong topic was specified, try one of available bellow:'
+                                .format())
+                            for key in self.json_object[argv[2]][argv[3]]:
+                                print(key)
+                        else:
+                             print("Multiple keys error!")
+            elif len(argv) < 3:
+                for key in self.json_object:
+                    print(key) 
+                        
+        except (IndexError):
+            print(IndexError)
