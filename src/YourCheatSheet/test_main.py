588c0f30ef498182d8f267276c07e24511efcaa0
Max <profex55@gmail.com>
2023-10-13 18:58:59 -0500

    modified tests

--- src/YourCheatSheet/test_main.py
+++ src/YourCheatSheet/test_main.py
@@ -8,10 +8,20 @@ def docs():
 
 def test_parser_single_key(docs, capsys): 
     docs.get_data("GIT")
+    out, err = capsys.readouterr()
+    assert 'Config' in out
+
+def test_parser_single__wrong_key(docs, capsys): 
+    docs.get_data("GiT")
     captured = capsys.readouterr()
-    assert 'Config' in captured.out
+    assert 'Wrong topic' in captured.out 
 
 def test_parser_multiple_keys(docs, capsys):
     docs.get_data("Python","Tests")
     captured = capsys.readouterr()
     assert 'Fixture' in captured.out 
+
+def test_parser_multiple_wrong_keys(docs, capsys):
+    docs.get_data("Pyhon","Slices")
+    captured = capsys.readouterr()
+    assert 'keys error' in captured.out 
