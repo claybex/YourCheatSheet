e498fd89bb382f32db1dfc9b75e82c8081d89c8e
Max <profex55@gmail.com>
2023-10-13 20:20:47 -0500

    if k2 in k3 feature

--- src/YourCheatSheet/main.py
+++ src/YourCheatSheet/main.py
@@ -31,13 +31,26 @@ class Parser:
                             for key in self.json_object[k1][k2]:
                                 print(key)
                     except(KeyError, TypeError):
-                        if k1 in self.json_object:
-                            print('Wrong topic was specified, try one of available bellow:'
-                                .format())
-                            for key in self.json_object[k1]:
-                                print(key)
+                        def check_k2_in_k3():
+                            for key in self.json_object.get(k1, {}):
+                                try:
+                                    for key2, value in self.json_object.get(k1, {}).get(key, {}).items():
+                                        if key2 == k2:
+                                            return (value)
+                                            
+                                except AttributeError:
+                                    continue
+                        value = check_k2_in_k3()
+                        if value: 
+                            print(value)
                         else:
-                             print("Multiple keys error!")
+                            if k1 in self.json_object:
+                                print('Wrong topic was specified, try one of available bellow:'
+                                    .format())
+                                for key in self.json_object[k1]:
+                                    print(key)
+                            else:
+                                print("Multiple keys error!")
             elif k1 and k2 and k3:
                 try:
                     print(self.json_object[k1][k2][k3])
